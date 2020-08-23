0bb1fc27d980f9eb07dd71458cbe48da403de396
dennis2311 <58165780+dennis2311@users.noreply.github.com>
2020-08-23 15:37:53 +0900

    ~#6.1 (without LogIn, no 'search' page appears)

--- src/Components/Routes.js
+++ src/Components/Routes.js
@@ -3,22 +3,27 @@ import React from "react";
 import { Route, Switch } from "react-router-dom";
 import Feed from "../Routes/Feed";
 import Auth from "../Routes/Auth/AuthContainer";
+import Explore from "../Routes/Explore";
+import Search from "../Routes/Search";
+import Profile from "../Routes/Profile";
 
 const LoggedInRoutes = () => (
-  <>
+  <Switch>
     <Route exact path="/" component={Feed}></Route>
-  </>
+    <Route path="/explore" component={Explore}></Route>
+    <Route path="/search" component={Search}></Route>
+    <Route path="/:username" component={Profile}></Route>
+  </Switch>
 );
 
 const LoggedOutRoutes = () => (
-  <>
+  <Switch>
     <Route exact path="/" component={Auth}></Route>
-  </>
+  </Switch>
 );
 
-const AppRouter = ({ isLoggedIn }) => (
-  <Switch>{isLoggedIn ? <LoggedInRoutes /> : <LoggedOutRoutes />}</Switch>
-);
+const AppRouter = ({ isLoggedIn }) =>
+  isLoggedIn ? <LoggedInRoutes /> : <LoggedOutRoutes />;
 
 AppRouter.propTypes = {
   isLoggedIn: PropTypes.bool.isRequired,
